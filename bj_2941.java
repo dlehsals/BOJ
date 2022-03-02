a47eb698c0a392f36a9a86d179dacbbe4271133d
Donmin Lee <76255761+dlehsals@users.noreply.github.com>
2022-03-02 09:45:58 +0900

    Add files via upload
    백준-크로아티아알파벳(문자열)

---
+++ bj_2941.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_2941 {
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        count = 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == 'c' && i < str.length() - 1){
+                if(str.charAt(i+1) == '=' || str.charAt(i+1) == '-'){
+                    i++;
+                }
+            }
+            else if(str.charAt(i) == 'd' && i < str.length() - 1){
+                if(str.charAt(i+1) == '-')
+                {
+                    i++;
+                }
+                else if(str.charAt(i+1) == 'z' && i < str.length() - 2){
+                    if(str.charAt(i+2) == '='){
+                        i+=2;
+                    }
+                }
+            }
+            else if((str.charAt(i) == 'l' || str.charAt(i) == 'n') && i < str.length() - 1){
+                if(str.charAt(i+1) == 'j'){
+                    i++;
+                }
+            }
+            else if((str.charAt(i) == 's' || str.charAt(i) == 'z') && i < str.length() - 1){
+                if(str.charAt(i+1) == '='){
+                    i++;
+                }
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
