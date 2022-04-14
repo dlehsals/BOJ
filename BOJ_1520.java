bea82cdf029a6c477bf1e410bed4263241f0e3a6
Donmin Lee <76255761+dlehsals@users.noreply.github.com>
2022-04-14 17:14:36 +0900

    백준-내리막길(DFS+DP)

---
+++ BOJ_1520.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_1520 {
+    static int m, n;
+    static int[][] map;
+    static int[][] dp;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static public class Pair{
+        int x;
+        int y;
+        int height;
+        Pair(int x, int y, int height){
+            this.x = x;
+            this.y = y;
+            this.height = height;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        map = new int[m][n];
+        dp = new int[m][n];
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0;j<n;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i], -1);
+        }
+
+        dfs(0, 0);
+
+        System.out.println(dp[0][0]);
+    }
+
+    public static int dfs(int x, int y){
+        if(x == m-1 && y == n-1) return 1;
+
+        if(dp[x][y] != -1) return dp[x][y];
+
+        dp[x][y] = 0;
+        for(int i=0;i<4;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+            if(map[x][y] > map[nx][ny])
+                dp[x][y] += dfs(nx, ny);
+        }
+        return dp[x][y];
+    }
+    public static void printMap(){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
