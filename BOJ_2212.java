bb06fdeebb1f9dda752f824b6a8d0295d4b7c2e9
Donmin Lee <76255761+dlehsals@users.noreply.github.com>
2022-05-03 16:18:56 +0900

    Update BOJ_2212.java
    [생각해야 될 것]
    행복 유치원 문제랑 똑같음.
    
    n개의 집중국이 있으면 k개의 센서를 이용해서 모든 집중국을 커버한다 -> 그룹화를 하면서 집중국 간의 거리를 최소화 해라는 소리
    
    우선 좌표가 중구난방으로 입력되니까 정렬해주고
    
    i번째와 i+1번째의 차이를 담을 diff배열을 만들어준다.
    
    만약 [1, 3, 5, 7, 9]라는 입력이 주어졌고, 그룹을 (1, 5) 로 하게 된다면
    
    (1, 5)의 값 = 4가 된다.
    이 4는 사실 (1,3) 그룹과 (3, 5)그룹의 합과 같다.
    
    이 말은 모든 좌표 인덱스에 대해서 차이를 구해놓으면
    
    그룹을 어떻게 짓든간에 최소값을 구할 수 있다는 sori.
    
    제일 확실한 건 그림을 그리면서 식을 세우면 쉽게 풀 수 있다.
    
    diff배열은 꼭 오름차순으로 정렬을 한다.
    
    만약 6개의 집중국을 2개의 센서로 커버친다고 할 때,
    
    diff는 5개가 나오게 되고, 그 값들을 정렬하게 되면
    
    차이가 최소인 집중국부터 구할 수 있다.
    
    예시)
    n = 6, k = 2
    
    집중국 좌표 = [1, 3, 6, 6, 7, 9]
    
    diff = [2, 3, 0, 1, 2]
    
    diff 정렬 후 = [0, 1, 2, 2, 3]
    
    n-k개를 뽑는다? 6-2 -> 4개
    
    0, 1, 2, 2를 뽑는 경우는
    
    (6, 6, 7, 9) (1, 3)이렇게 짝지어 짐.
    
    다 더하면 정답은 5

--- BOJ_2212.java
+++ BOJ_2212.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+//
 public class BOJ_2212 {
     static int n, k;
     static int[] arr;
